9ec7b10cf9de781362f93a837106d1b56afc7c43
sadhika7 <swatiadhikari1997@gmail.com>
2020-07-28 15:58:33 -0500

    Fixed PORT error for heroku deployment

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ mongoose
     console.log('DB connection successful!');
   });
 const app = require('./app');
-const port = process.env.port || 3000;
+
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
